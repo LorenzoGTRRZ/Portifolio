bf158b21938d49f6fb39463f1cec668673103f74
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-03-19 15:52:07 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/LorenzoGTRRZ.png" />
+    <Titulo fontSize={20}>Lorenzo Gutierrez</Titulo>
+  </aside>
+)
+
+export default Sidebar
